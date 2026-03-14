f560a98462bd766d416422be18b458b0b77e3500
ToJaVadym <vadymivaneiko2@gmail.com>
2026-03-14 20:13:21 +0100

    Changed code

--- app/main.py
+++ app/main.py
@@ -4,25 +4,24 @@ class Person:
     def __init__(self, name: str, age: int):
         self.name = name
         self.age = age
-        Person.people[name]= self
+        Person.people[name] = self
+        self.spouse = None
 
-def create_person_list(people: list) -> list:
-    for p in people:
+
+def create_person_list(people_data: list) -> list:
+    Person.people.clear()
+
+    for p in people_data:
         Person(p['name'], p['age'])
 
     result_list = []
-    for p in people:
+    for p in people_data:
         current_person = Person.people[p['name']]
 
         spouse_name = p.get('wife') or p.get('husband')
 
-        spouse_name = p.get('wife')
-        if spouse_name is None:
-            spouse_name = p.get('husband')
-
         if spouse_name and spouse_name in Person.people:
-            attr_name = 'wife' if p.get('wife') is not None else 'husband'
-            setattr(current_person, attr_name, Person.people[spouse_name])
+            current_person.spouse = Person.people[spouse_name]
 
         result_list.append(current_person)
 
